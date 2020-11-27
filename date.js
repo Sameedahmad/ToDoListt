298db3d5021ac1a96d16114536a966a7a3683d4b
Sameedahmad <53148681+Sameedahmad@users.noreply.github.com>
2020-11-27 20:16:42 +0500

    Initial Upload.
    ToDo List App deployed with Heroku and database managed by Mongo Atlas.

---
+++ date.js
@@ -0,0 +1,35 @@
+module.exports.getDate = function (){
+
+    var today = new Date();
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month:"long"
+    };
+
+    var day = today.toLocaleDateString("en-GB", options);
+
+    return day;
+
+}
+
+
+module.exports.getDay = function (){
+
+    var today = new Date();
+    
+    var options = {
+        weekday: "long",
+      
+    };
+
+    var day = today.toLocaleDateString("en-GB", options);
+
+    return day;
+
+}
+
+
+
+
